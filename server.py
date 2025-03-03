42e91cc6b30b8945864ba3ee85f65aea5a4b573e
lujiaji-5pro <fxmxjx720@gmail.com>
2025-03-02 21:57:15 -0800

    elec

--- server.py
+++ server.py
@@ -2,7 +2,9 @@ from tinydb import TinyDB, Query
 import threading
 import socket
 import json
-
+import time
+import hashlib
+import random
 # "server_info":{
 #         "server_id": "S1",
 #         "cluster": 1,
@@ -11,14 +13,19 @@ import json
 #         "db_file": "data/db_server_S1.json"
 #     }
 class Server:
-    def __init__(self, server_info):
+    def __init__(self, server_info,servers):
         # Initialize some parameters
         self.server_id = server_info["server_id"]
         self.cluster = server_info["cluster"]  # Needs to be initialized
         self.ip = server_info["ip"]
         self.port = server_info["port"]
         self.db_file = server_info["db_file"]
+        self.others=[]
 
+        for server_info in servers:
+            if server_info["server_id"]!=self.server_id and server_info["cluster"]==self.cluster:
+                self.others.append(server_info)
+        print(f"{self.server_id}:{self.others}")
 
         self.id = None  # Read from JSON
         self.data_base = self.initMyStorage(self.db_file)
@@ -31,7 +38,7 @@ class Server:
 
 
         threading.Thread(target = lambda : self.listening()).start()
-        threading.Thread(target = lambda : self.election()).start()
+        threading.Thread(target = lambda : self.monitor_timeout()).start()
 
         
         #------------ 以上已完成初始化，以下是需要实现的 ------------
@@ -46,9 +53,13 @@ class Server:
         self.cur_index = [0, 0]  # [term, index]
         self.match_index = [0, 0]  # Already replicated on server
         self.log_temp = None # 临时存储log
+        self.timeout_limit=1
+        self.last_message_time=time.time()
+        self.leader_exist=0
 
     def listening(self):
         while True:
+            print(f"[{self.server_id}] listened")
             client_socket, address = self.socket.accept()
             while True:
                 data = client_socket.recv(1024).decode()
@@ -56,13 +67,15 @@ class Server:
                 # mySleep(self.sleepType)
                 if not data:
                     break
-                msg_data = json.loads(data)
-                if (msg_data["code"] == "check"):
-                  print("check")
-                if (msg_data["code"] == "check_rply"):
-                   print("check_rply")
-                if (msg_data["code"] == "release"):
-                    print("release")
+                else:
+                    self.last_message_time=time.time()
+                    msg_data = json.loads(data)
+                    if (msg_data["type"] == "elec"):
+                        print(f"[{self.server_id}] receive elec,need to vote!")
+                    # if (msg_data["code"] == "check_rply"):
+                    #     print("check_rply")
+                    # if (msg_data["code"] == "release"):
+                    #     print("release")
 
 
     def initMyStorage(self, db_file_name):
@@ -76,10 +89,39 @@ class Server:
         return db
     
     """Chain functions according to execution order"""
-    def election(self, target):
-        # message = Message({"""Refer to the format of elec"""})
+    def election(self):
+        message={
+            "type":"elec",
+            "tran":{
+                "term":self.cur_term,
+                "candidated_id":self.server_id,
+                "last_included_index":self.commit_index,
+                "last_included_term":self.cur_term+1
+                },
+            "mid":hashlib.md5(str(random.random()).encode()).hexdigest(),
+            }
+        message=json.dumps(message)
+        for other in self.others:
+            my_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            my_socket.connect((other["ip"],int(other["port"])))
+            my_socket.send(message.encode())
+            my_socket.close()
+
         # for client in self.cluster:
         #     message.sendTo(client)
+
+        return
+
+    def monitor_timeout(self):
+        while True:
+            time.sleep(0.5)
+            time_gap=time.time()-self.last_message_time
+            if time_gap>self.timeout_limit:
+                print(f"[{self.server_id}] long time no receive msgs,timeout!")
+                self.leader_exist=0
+                self.leader=None
+                self.election()
+
     # def receiveMsgs(self):
     # """Divided into three situations: "election", "normal", "C_change",
     # Can be distinguished by the outermost key of the msg's dict
@@ -92,7 +134,7 @@ class Server:
     #         case "committed":
     #             do self.update()
     # """
-        return
+
     # def receiveClientRequest(self):
 
     # def sendRequest(self):
